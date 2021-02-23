e1590dd00ce8a823880440f18d48c8108b11d6cb
Christian Matzner <cmatzner.sch@gmail.com>
2021-02-23 14:48:21 +0100

    first commit

---
+++ Control_Horas/Inicio.Designer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.Titulo_Inicio = new System.Windows.Forms.Label();
+            this.Nombre_txt = new System.Windows.Forms.Label();
+            this.Horas_inicio_txt = new System.Windows.Forms.Label();
+            this.Cerrar_Inicio = new System.Windows.Forms.Button();
+            this.Texto_Nombre = new System.Windows.Forms.Label();
+            this.Hours_Text = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Titulo_Inicio
+            // 
+            this.Titulo_Inicio.AllowDrop = true;
+            this.Titulo_Inicio.AutoEllipsis = true;
+            resources.ApplyResources(this.Titulo_Inicio, "Titulo_Inicio");
+            this.Titulo_Inicio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Titulo_Inicio.Name = "Titulo_Inicio";
+            // 
+            // Nombre_txt
+            // 
+            resources.ApplyResources(this.Nombre_txt, "Nombre_txt");
+            this.Nombre_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Nombre_txt.Name = "Nombre_txt";
+            // 
+            // Horas_inicio_txt
+            // 
+            resources.ApplyResources(this.Horas_inicio_txt, "Horas_inicio_txt");
+            this.Horas_inicio_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Horas_inicio_txt.Name = "Horas_inicio_txt";
+            // 
+            // Cerrar_Inicio
+            // 
+            resources.ApplyResources(this.Cerrar_Inicio, "Cerrar_Inicio");
+            this.Cerrar_Inicio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Cerrar_Inicio.Name = "Cerrar_Inicio";
+            this.Cerrar_Inicio.UseVisualStyleBackColor = true;
+            this.Cerrar_Inicio.Click += new System.EventHandler(this.Cerrar_Inicio1);
+            // 
+            // Texto_Nombre
+            // 
+            resources.ApplyResources(this.Texto_Nombre, "Texto_Nombre");
+            this.Texto_Nombre.Name = "Texto_Nombre";
+            // 
+            // Hours_Text
+            // 
+            resources.ApplyResources(this.Hours_Text, "Hours_Text");
+            this.Hours_Text.Name = "Hours_Text";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            resources.ApplyResources(this.notifyIcon1, "notifyIcon1");
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Captura.PNG");
+            this.imageList1.Images.SetKeyName(1, "3.png");
+            // 
+            // Inicio
+            // 
+            this.AllowDrop = true;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Hours_Text);
+            this.Controls.Add(this.Texto_Nombre);
+            this.Controls.Add(this.Cerrar_Inicio);
+            this.Controls.Add(this.Horas_inicio_txt);
+            this.Controls.Add(this.Nombre_txt);
+            this.Controls.Add(this.Titulo_Inicio);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.MaximizeBox = false;
+            this.Name = "Inicio";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Titulo_Inicio;
+        private System.Windows.Forms.Label Nombre_txt;
+        private System.Windows.Forms.Label Horas_inicio_txt;
+        private System.Windows.Forms.Button Cerrar_Inicio;
+        //private System.Windows.Forms.Button Boton_Prueba;
+        private System.Windows.Forms.Label Texto_Nombre;
+        private System.Windows.Forms.Label Hours_Text;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+
+
+    class Dato
+    {
+        private String dato;
+
+        public void setDato(String dato)
+        {
+            this.dato = dato;
+        }
+        public String getDato()
+        {
+            return this.dato;
+        }
+    }
+}
+
---
+++ Control_Horas/Inicio.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Tulpep.NotificationWindow;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Inicio : Form
+    {
+
+        //--------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        //---------------------------------------INICIA EL PROCESO------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        /*public static void showNotification(int days)
+        {
+            Notificacion VentanaNotificacion = new Notificacion(days);
+            VentanaNotificacion.Show();---
+            PopupNotifier popup = new PopupNotifier();
+            popup.TitleText = "Alerta de deias";
+            popup.ContentText = "7 dias de trabajo";
+            popup.AnimationDuration = 1000;
+
+            popup.Popup();
+        }*/
+
+        public Inicio()
+        {
+            string connectionString = "Server = DESKTOP-P78S0RT; Database = AxTrax1; Trusted_Connection = True;";
+            InitializeComponent();
+            SqlDependency.Stop(connectionString);
+            SqlDependency.Start(connectionString);
+            RegisterForChanges();
+        }
+
+        //--------------------------------------------------------------------------------------------------------
+        //---------------------------------------CAPTA CAMBIO EN BASE DE DATOS------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+
+        public void RegisterForChanges()
+        {
+            SqlConnection conectarbd = new SqlConnection("Server = DESKTOP-P78S0RT; Database = AxTrax1; Trusted_Connection = True;");
+            conectarbd.Open();
+            string consulta1 = "select tFullName from dbo.tblEvents";
+            try
+            {
+                SqlCommand comando = new SqlCommand(consulta1, conectarbd);
+                SqlDependency oDependency = new SqlDependency(comando);
+                oDependency.OnChange += new OnChangeEventHandler(OnNotificationChange);
+                SqlDataReader objReader = comando.ExecuteReader();
+                try
+                {
+
+                }
+                finally
+                {
+                    objReader.Close();
+                }
+            }
+            finally
+            {
+                conectarbd.Close();
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------------------
+        //---------------------------------------ACTUALIZA TEXTO EN APLICACIÓN------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+
+        public void OnNotificationChange(object caller, SqlNotificationEventArgs e)
+        {
+            string name;
+            int days;
+            int rows;
+            string connectionString = "Server = DESKTOP-P78S0RT; Database = AxTrax1; Trusted_Connection = True;";
+            name = ExtractName(connectionString);
+            rows = ExtractRows(connectionString, name);
+            days = ExtractDate(connectionString, name, rows);
+            // Testisting if getting dependency response...
+            Texto_Nombre.Invoke((MethodInvoker)delegate
+            {
+                Texto_Nombre.Text = name;
+                Hours_Text.Text = days.ToString();
+            });
+            RegisterForChanges();
+            pictureBox1_Click(days);
+        }
+
+        //--------------------------------------------------------------------------------------------------------
+        //---------------------------------------EXTRAE NOMBRE DE BASE DE DATOS-----------------------------------
+        //--------------------------------------------------------------------------------------------------------
+
+        private static string ExtractName(string connectionString)
+        {
+            string name = "No hubo cambios";
+            string queryString = "Select tFullName From tblEvents order by dtEventUpload desc";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString, connection);
+            connection.Open();
+            SqlDataReader rdr = command.ExecuteReader();
+            // Call Read before accessing data. 
+            if (rdr.HasRows)
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    rdr.Read();
+                    name = rdr.GetString(0);
+                }
+                // Call Close when done reading.
+                rdr.Close();
+            }
+            return name;
+        }
+
+        //--------------------------------------------------------------------------------------------------------
+        //---------------------------------------EXTRAE FECHA DE BASE DE DATOS------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        public static int ExtractDate(string connectionString, string nombre, int rows)
+        {
+            double hrsDif = 0;
+            int totalDays = 0;
+            DateTime[] registerHour = new DateTime[rows];
+            var dateString = "1/1/2000 12:00:00";
+            DateTime storedHour = DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+            string queryString_dates = "Select dtEventUpload From tblEvents where tFullName = '" + nombre + "' order by dtEventUpload desc";
+            string queryString_rows = "SELECT count(dtEventUpload) FROM tblEvents WHERE tFUllName= '" + nombre + "'";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString_dates, connection);
+            connection.Open();
+            SqlDataReader rdr = command.ExecuteReader();
+            // Call Read before accessing data.
+
+            for (int dayCount = 0; dayCount < rows; dayCount++)
+            {
+                if (rdr.HasRows)
+                {
+                    rdr.Read();
+                    registerHour[dayCount] = rdr.GetDateTime(0);
+                    if (dayCount == 0)
+                    {
+                        storedHour = registerHour[dayCount];
+                    }
+                    else
+                    {
+                        hrsDif = (Math.Abs(storedHour.Day - registerHour[dayCount].Day));
+                        if (hrsDif == 30 || hrsDif == 29 || hrsDif == 28)
+                        {
+                            hrsDif = 1;
+                        }
+                        if (hrsDif == 1)
+                        {
+                            totalDays++;
+                            storedHour = registerHour[dayCount];
+                        }
+                        if (hrsDif > 1)
+                        {
+                            break;
+                        }
+                        if (totalDays >= 7)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            rdr.Close();
+            connection.Close();
+            return totalDays;
+        }
+
+        public static int ExtractRows(string connectionString, string nombre)
+        {
+            int rows = 0;
+            string queryString_rows = "SELECT count(dtEventUpload) FROM tblEvents WHERE tFUllName= '" + nombre + "'";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(queryString_rows, connection);
+            connection.Open();
+            SqlDataReader rdr = command.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                rdr.Read();
+                rows = rdr.GetInt32(0);
+            }
+            rdr.Close();
+            connection.Close();
+            return rows;
+        }
+
+
+        //--------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        //----------------------------------------SE ARMA LA INTERFAZ---------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------
+
+
+
+
+        /*public static void openNotification(EventArgs e, int days)
+        {
+            if (days >= 7)
+            {
+                Notificacion VentanaNotificacion = new Notificacion(days);
+                VentanaNotificacion.Show();
+            }
+        }*/
+
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cerrar_Inicio1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(int days)
+        {
+            if (days >= 7)
+            {
+                pictureBox1.Image = imageList1.Images[1];
+            }
+            else
+            {
+                pictureBox1.Image = imageList1.Images[0];
+            }
+        }
+
+        /* private void Boton_Prueba1(object sender, EventArgs e)
+         {
+             using (Notificacion VentanaNotificacion = new Notificacion(Texto_Horas.Text))
+                 VentanaNotificacion.ShowDialog();*/
+    }
+}
